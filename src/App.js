9f9aeb3d8ea0b35815c4ccae00c81476da39d604
willyao666 <13605529+willyao666@user.noreply.gitee.com>
2024-01-18 15:12:45 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {HashRouter, Routes, Route} from "react-router-dom"
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Hotel from "./pages/hotel/Hotel";
 
 function App() {
   return(
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/hotels" element={<List/>}/>
       <Route path="/hotels/:id" element={<Hotel/>}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
